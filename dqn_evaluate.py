2c3beacd534d9571072e6644f8ed2d8186129436
YashSax <yash.saxena@gmail.com>
2024-04-23 00:56:04 -0500

    Fixed DQN evaluate

--- dqn_evaluate.py
+++ dqn_evaluate.py
@@ -7,4 +7,8 @@ agent = Agent(gamma=0.99, epsilon=0, lr=0.001,
                       input_dims=[5], batch_size=64, n_actions=4)
 agent.q_eval.load_state_dict(torch.load(MODEL_DIR))
 
-run_episode(policy=lambda observation: agent.choose_action(observation), render=True)
+policy = lambda observation: agent.choose_action(observation)
+cum_reward = run_episode(policy=policy, render=True)
+print("Example episode Reward:", cum_reward)
+
+evaluate_policy(policy)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from rocketgym_local.environment import Environment
+from utils import *
+from tqdm import tqdm
 
 ACTION_LEFT = 0
 ACTION_MID = 1
@@ -22,3 +24,13 @@ def run_episode(policy, render=True):
             env.render()
 
     return cum_reward
+
+def evaluate_policy(policy, num_episodes=100):
+    cum_rewards = []
+    for _ in tqdm(range(num_episodes)):
+        cum_reward = run_episode(policy, render=False)
+        cum_rewards.append(cum_reward)
+
+    print("Average Cumulative Reward:", sum(cum_rewards) / num_episodes)
+    print("Best performance:", max(cum_rewards))
+    print("Worst performance:", min(cum_rewards))
